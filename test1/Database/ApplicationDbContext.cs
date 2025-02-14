08aa5d7480ce98ff8133401a83f6089d1524fd0a
Nishant Shingate <nishantks121@gmail.com>
2025-02-14 23:19:19 +0530

    added required packages

---
+++ test1/Database/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace test1;
+// public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+// {
+
+// }
--- test1/Program.cs
+++ test1/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "";
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
