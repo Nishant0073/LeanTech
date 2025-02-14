d66164998a3b24e13a9da2d5703fc650fcc8ac59
Nishant Shingate <nishantks121@gmail.com>
2025-02-14 22:05:39 +0530

    first commit

---
+++ test1/Controller/Test.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace test1;
+
+[ApiController]
+[Route("controller")]
+public class Test : ControllerBase
+{
+		[HttpGet("TestAPI")]
+		public async Task<IActionResult> Testing()
+		{
+				return Ok("Testing passed!");
+		}
+
+}
