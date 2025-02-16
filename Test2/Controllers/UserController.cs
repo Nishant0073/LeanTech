29408ed6c36291660ab2c1f25a3bd77b433ea350
Nishant Shingate <nishantks121@gmail.com>
2025-02-16 23:07:08 +0530

    changes for database and packages

---
+++ Test2/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test2;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController: ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetToken(){
+        return Ok("Testing.. ");
+    }
+
+}
