025971be304857d483ef1ef8c9901da296dc0d59
Nishant Shingate <nishantks121@gmail.com>
2025-02-17 22:34:44 +0530

    Added Scoped

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -2,12 +2,20 @@ using DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddTransient<TestService>();
+//builder.Services.AddTransient<TestService>();
 /*
 {
   "service1": "99b5f491-92d1-426f-9019-4d26b196196d",
   "service2": "ff3b3035-9e0a-4ec8-adfd-7b1046a7da79"
 }*/
+
+builder.Services.AddScoped<TestService>();
+/*
+{
+  "service1": "9b7360d1-1fb8-4e08-b318-5610466d9388",
+  "service2": "9b7360d1-1fb8-4e08-b318-5610466d9388"
+}
+*/
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
