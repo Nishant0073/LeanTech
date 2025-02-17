2d1becf6c95a3f42a343349570597a58afd13d88
Nishant Shingate <nishantks121@gmail.com>
2025-02-17 22:36:38 +0530

    Added more request output

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -15,7 +15,12 @@ builder.Services.AddScoped<TestService>();
   "service1": "9b7360d1-1fb8-4e08-b318-5610466d9388",
   "service2": "9b7360d1-1fb8-4e08-b318-5610466d9388"
 }
+{
+  "service1": "e18bf0c0-fc3d-41c6-af57-3765866e338d",
+  "service2": "e18bf0c0-fc3d-41c6-af57-3765866e338d"
+}
 */
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
