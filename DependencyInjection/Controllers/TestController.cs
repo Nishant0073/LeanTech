d58800cd8e34624dfc80aeead0ee6e1618cba18a
Nishant Shingate <nishantks121@gmail.com>
2025-02-17 22:31:46 +0530

    added oouput

---
+++ DependencyInjection/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class TestController:ControllerBase
+{
+    private TestService _testService1;
+    private TestService _testService2;
+    public TestController(TestService testService1,TestService testService2)
+    {
+        _testService1 = testService1;
+        _testService2 = testService2;
+    }
+    
+    [HttpGet]
+    public IActionResult GetGuid(){
+        return Ok(
+        new {
+            Service1 = _testService1.GetGuid(),
+            Service2 = _testService2.GetGuid(),
+        }
+        );
+    }
+
+}
